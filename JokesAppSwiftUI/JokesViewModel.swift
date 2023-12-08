6c3bac5a0112f0d80bbc887e4f85208e80a8d4f4
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-08 20:46:43 +0300

    Alamofire added

--- JokesAppSwiftUI/JokesViewModel.swift
+++ JokesAppSwiftUI/JokesViewModel.swift
@@ -6,13 +6,30 @@
 //
 
 import Foundation
+import Alamofire
 
 class JokesViewModel : ObservableObject { //Gozlemlenebilir yapmak icin observable
     
     @Published var jokes = [Value]()  //contentview'da degisikligi gorebilmek icin
     
-    func getJokes(){
+    init(){
+        getJokes()
+    }
+    
+    func getJokes(count: Int = 6){
         
+        AF.request("https://raw.githubusercontent.com/atilsamancioglu/JokesAppJsonData/main/chuck.json", method: .get).responseDecodable(of: Welcome.self) { response in
+            
+            switch response.result {
+                
+            case.success(let data):
+                let value = data.value
+                self.jokes += value
+                
+            case.failure(let error):
+                print(error)
+            }
+        }     
     }
     
 }
