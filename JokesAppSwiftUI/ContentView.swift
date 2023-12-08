e4d6a26abd2cae2f1a303edca19e30a58a487cd1
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-08 20:57:32 +0300

    Codes added to view data

--- JokesAppSwiftUI/ContentView.swift
+++ JokesAppSwiftUI/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var jokesVM = JokesViewModel() 
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        NavigationView{
+            
+            List(jokesVM.jokes) { element in
+                
+                Text(element.joke)
+                
+            }
+            
+        }.navigationBarTitle(Text("Jokes App"))
     }
 }
 
